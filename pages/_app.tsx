06f631905a0d6c408e9f24920acf50174858db8e
Adrián coll <nege2002@gmail.com>
2023-03-30 02:09:20 +0200

    feat: use the providers and index page init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
+import { UiProvider } from '@/context'
+import { FirestoreProvider } from '@/context/firestore'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <FirestoreProvider>
+      <UiProvider>
+        <Component {...pageProps} />
+      </UiProvider>
+    </FirestoreProvider>
+  )
 }
