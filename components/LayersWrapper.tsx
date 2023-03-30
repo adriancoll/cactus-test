aa68771170eee89ca649c83034a6c1ea74dddcee
Adrián coll <nege2002@gmail.com>
2023-03-30 12:00:03 +0200

    feat: improve layers wrapper design

--- components/LayersWrapper.tsx
+++ components/LayersWrapper.tsx
@@ -1,33 +1,43 @@
 import { useUI } from '@/hooks/useUI'
 import Image from 'next/image'
-import React from 'react'
+import { CoordPoint } from './CoordPoint'
+import { useFirestore } from '@/hooks/useFirestore'
 
 const IMAGE_WIDTH = 1240
 const IMAGE_HEIGHT = 873
 
 export const LayersWrapper = () => {
+  const { points } = useFirestore()
   const { layers } = useUI()
 
   return (
-    <div className="rounded-3xl relative overflow-hidden">
-      <Image
-        className="select-none object-contain pointer-events-none"
-        priority
-        width={IMAGE_WIDTH}
-        height={IMAGE_HEIGHT}
-        src="/images/base.jpeg"
-        alt="Base image"
-      />
-      {Object.entries(layers).map(([pointId, image]) => (
+    <div className='flex'>
+      {/* material selector points */}
+      {points.map((point) => (
+        <CoordPoint key={point.name} point={point} />
+      ))}
+      <div className="rounded-3xl relative overflow-hidden w-fit">
+        {/*  background image */}
         <Image
-          className="absolute z-10 inset-0"
-          alt={pointId}
-          key={pointId}
-          src={image}
+          className="select-none object-contain pointer-events-none"
+          priority
           width={IMAGE_WIDTH}
           height={IMAGE_HEIGHT}
+          src="/images/base.jpeg"
+          alt="Base image"
         />
-      ))}
+        {/* Map the selected layers from ui context */}
+        {Object.entries(layers).map(([pointId, image]) => (
+          <Image
+            className="absolute z-10 inset-0"
+            alt={pointId}
+            key={pointId}
+            src={image}
+            width={IMAGE_WIDTH}
+            height={IMAGE_HEIGHT}
+          />
+        ))}
+      </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,21 @@
 import { Layout, LayersWrapper } from '@/components'
-import { CoordPoint } from '@/components/CoordPoint'
 import { useFirestore } from '@/hooks/useFirestore'
-import { useUI } from '@/hooks/useUI'
 
 export default function Home() {
-  const { layers } = useUI()
-  const { points, loading } = useFirestore()
+  const { loading } = useFirestore()
 
   if (loading) {
     return (
       <div className="h-screen bg-primary flex items-center justify-center">
-        <p className="text-center text-3xl font-bold text-white">Loading...</p>
+        <p className="text-center animate-fadeIn text-3xl font-bold text-white">Loading...</p>
       </div>
     )
   }
 
   return (
     <Layout>
-      <main className="container mx-auto pt-20">
+      <main className="container pt-40 h-screen mx-auto">
         <div className="relative">
-          {points.map((point) => (
-            <CoordPoint key={point.name} point={point} />
-          ))}
           <LayersWrapper />
         </div>
       </main>
