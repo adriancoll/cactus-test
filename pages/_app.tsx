47fca9d6577efd7da71c00dd22eccb06d2201cf0
Adrián coll <nege2002@gmail.com>
2023-03-31 12:10:02 +0200

    fix: global styles import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
 import { UiProvider, FirestoreProvider } from '@/lib/context'
 
-import '@/styles/globals.css'
+import '@/lib/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
